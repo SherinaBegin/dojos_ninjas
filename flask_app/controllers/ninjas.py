a5318f64757d8ceecf58438044fa980a7e1ae793
Sherina Begin <98630481+SherinaBegin@users.noreply.github.com>
2022-03-26 14:02:56 -0700

    adding ninja functions complete

--- flask_app/controllers/ninjas.py
+++ flask_app/controllers/ninjas.py
@@ -2,9 +2,19 @@ from flask import render_template, request, redirect
 
 from flask_app import app
 from flask_app.models.ninja import Ninja
+from flask_app.models.dojo import Dojo
 
-@app.route('/ninjas/add', methods=['POST'])
-def add_ninja():
+@app.route('/ninjas')
+def ninjas():
+   dojos = Dojo.get_all()
+   print(dojos)
+   return render_template('add_ninja.html', dojos = dojos)
+
+@app.route('/ninjas/save', methods=['POST'])
+def save_ninja():
    print(request.form)
-   Ninja.save_ninja(request.form)
+   Ninja.create_ninja(request.form)
    return redirect('/')
+
+
+
